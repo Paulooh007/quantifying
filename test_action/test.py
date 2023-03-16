fc0e14ed63afd73bef3f7eeeccf34265a639ed85
paul <okewunmipaul@yahoo.com>
2023-03-16 10:38:31 +0100

    Test action

--- test_action/test.py
+++ test_action/test.py
@@ -1,14 +1,16 @@
 import datetime
-
+import os
 # Get the current date and time
 now = datetime.datetime.now()
+
+CWD = os.path.dirname(os.path.abspath(__file__))
 
 # Format the date and time as a string
 timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
 
 # Create the filename with the current datetime
 filename = f"{timestamp}.txt"
-
+test_path = os.path.join(CWD, filename)
 # Open the file and write today's date to it
-with open(filename, "w") as f:
+with open(test_path, "w") as f:
     f.write(now.strftime("%Y-%m-%d"))
